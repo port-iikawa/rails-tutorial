17b57e826acd8ba464882b4109ddd1ada4e1ffb9
port-iikawa <shoya.iikawa@theport.jp>
2024-04-24 18:08:49 +0900

    finish sample_app

--- environment/sample_app/config/routes.rb
+++ environment/sample_app/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
@@ -7,5 +6,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "application#hello"
+  root "static_pages#home"
 end
