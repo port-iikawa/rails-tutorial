1dc740f101b58a2c94a8a79c6f214b7fa9a0e3a6
port-iikawa <shoya.iikawa@theport.jp>
2024-06-04 18:16:42 +0900

    11

--- environment/sample_app/app/mailers/application_mailer.rb
+++ environment/sample_app/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "user@realdomain.com"
+  default from: "postmaster@sandbox5205f6b7fad54ca19c10a00589e06ef1.mailgun.org"
   layout "mailer"
 end
